432b441fc76ccf2e6eef94f08fd3224d20203998
raviansyahyp <raviansyahy@gmail.com>
2021-11-10 19:11:16 +0700

    penambahan role

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -148,4 +148,9 @@ class StudentController extends Controller
     { 
         $student = Student::find($id); $pdf = PDF::loadview('students.report',['student'=>$student]); return $pdf->stream(); 
     }
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 }
